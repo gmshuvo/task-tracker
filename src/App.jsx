92bac903c9ebd893dfdb6fe8cb3480d0946cd074
Gm-shuvo <gm.shuvo.ru.cse@gmail.com>
2023-10-21 11:56:57 +0600

    code starter

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="">
+      <h1 className='text-3xl text-red-400'>My React App</h1>
+    </div>
+  );
+}
+
+export default App;
